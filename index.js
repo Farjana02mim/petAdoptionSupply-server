ce0da69475c047707c29ab344bf1d9fe29397050
Farjana Akter Mim <farjanaaktermim330@gmail.com>
2025-12-10 04:24:42 +0600

    recheck all api

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Server is running very well!");
+  res.send("Server is running fine!");
 });
 
 app.listen(port, () => {
